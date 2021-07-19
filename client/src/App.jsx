ee1e944933f3eb41f34d9ea6bbfc9a2c9180b3aa
Jason Melnikel <jmelnikel@gmail.com>
2021-07-19 13:43:24 -0400

    trying to fix deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,16 @@ const App = () => {
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
-            <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              component={UpdatePage}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantDetailPage}
+            />
           </Switch>
         </Router>
       </div>
