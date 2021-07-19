c50ed31596874a574f851b8a73595ef8dc54a5a2
Jason Melnikel <jmelnikel@gmail.com>
2021-07-19 14:42:28 -0400

    trying to fix deploy

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <h1 className="font-weight-light display-1 text-center">Restaurant Finder</h1>
+    <h1 className="font-weight-light display-1 text-center">Yelper-licious</h1>
   )
 };
 
